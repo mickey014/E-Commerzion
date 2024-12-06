0ddde1c3a411ba3a2f0b7a101c4ee856136aa3da
Kirk Anthony Mendoza <kirkmendoza9@gmail.com>
2024-12-06 16:58:44 +0800

    testing

--- user-product-service/product-service/src/test/java/com/codewithkirk/productService/IntegrationTesting/ProductServiceIntegrationTest.java
+++ user-product-service/product-service/src/test/java/com/codewithkirk/productService/IntegrationTesting/ProductServiceIntegrationTest.java
@@ -54,7 +54,7 @@ public class ProductServiceIntegrationTest extends ProductServiceIntegrationTest
     @Autowired
     private ProductServiceImp productServiceImp;
 
-    private Long userId = 1L;
+    private Long userId = 2L;
     private Long sellerId = 1L;
     private Long productId = 1L;
 
@@ -161,6 +161,7 @@ public class ProductServiceIntegrationTest extends ProductServiceIntegrationTest
         ProductPurchaseDto productPurchaseDto = new ProductPurchaseDto();
         productPurchaseDto.setOrderId(orderId);
         productPurchaseDto.setCustomerId(userId);
+        productPurchaseDto.setProductId(productId);
         productPurchaseDto.setOrderStatus("Pending");
         productPurchaseDto.setShippingAddress("1234 Elm Street, Some City, Some Country");
         productPurchaseDto.setPaymentMethod("Credit Card");
@@ -171,7 +172,7 @@ public class ProductServiceIntegrationTest extends ProductServiceIntegrationTest
 
         List<ProductItemsDto> orderItems = new ArrayList<>();
         // Adding multiple ProductItemsDto objects to the list
-        orderItems.add(new ProductItemsDto(productId, 2, BigDecimal.valueOf(50.25)));
+        orderItems.add(new ProductItemsDto(productId, 1, BigDecimal.valueOf(50.25)));
         orderItems.add(new ProductItemsDto(productId, 1, BigDecimal.valueOf(50.25)));
         productPurchaseDto.setOrderItems(orderItems);
 
